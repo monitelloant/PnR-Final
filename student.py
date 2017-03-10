941b7023300c0c7ec6bef24cf67fccfed1ec385e
monitelloant <amonitello@gmail.com>
2017-03-10 10:51:38 -0500

    Nav V.2

--- student.py
+++ student.py
@@ -196,22 +196,26 @@ class GoPiggy(pigo.Pigo):
     ### (kind of a big deal)
     ########################
 
-    def nav(self):
+    def final(self):
         print("-----------! NAVIGATION ACTIVATED !------------\n")
         print("[ Press CTRL + C to stop me, then run stop.py ]\n")
         print("-----------! NAVIGATION ACTIVATED !------------\n")
         # this is the loop part of the "main logic loop"
         while True:
-            while self.is_clear():
-                self.encF(10)
-            self.restore_heading()
+            if self.is_clear():
+                self.cruise()
             answer = self.choose_path()
             if answer == "left":
                 self.encL(6)
             elif answer == "right":
                 self.encR(6)
 
-
+    def cruise(self):
+        self.fwd()  # I added this to pigo
+        while self.is_clear():
+            time.sleep(.1)
+        self.stop()
+        self.encB(3)
 
     def encR(self, enc):
         pigo.Pigo.encR(self, enc)
